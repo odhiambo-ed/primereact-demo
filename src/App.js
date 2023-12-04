0f57ed336a57e42efbf1c8e76be8ceb1e2593bc0
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-12-04 12:01:03 +0300

    add filter function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 import data from './data';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { useState } from 'react';
+import { FilterMatchMode } from 'primereact/api';
 
 
 function App() {
+  const [filter, setFilter] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  });
   return (
     <div className="App">
+      <div>
+
+      </div>
       <DataTable value={data}>
         <Column field="id" header="ID" sortable></Column>
         <Column field="name" header="Name"></Column>
