0958c20745231201b5c2354fbb262f8c426d6909
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-12-04 11:43:34 +0300

    add datatable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import data from './data';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello Primereact</h1>
+      <DataTable value={data}>
+        <Column field="id" header="ID"></Column>
+        <Column field="name" header="Name"></Column>
+        <Column field="age" header="Age"></Column>
+        <Column field="city" header="City"></Column>
+      </DataTable>
     </div>
   );
 }
