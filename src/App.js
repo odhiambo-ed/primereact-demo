62a81d68feb6448d9c8e0bf05c98ff67c3d4070c
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-12-04 13:55:43 +0300

    add password gauge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import data from './data';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { useState } from 'react';
 import { FilterMatchMode } from 'primereact/api';
 import { InputText } from 'primereact/inputtext';
-
+import { Password } from 'primereact/password';
 
 function App() {
   const [filter, setFilter] = useState({
@@ -18,13 +20,16 @@ function App() {
         <InputText value={filter.global.value} onChange={(e) => setFilter({ ...filter, global: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS }, })} />
       </div>
       <DataTable value={data} filters={filter}
-        paginator rows={3} rowsPerPageOptions={[10, 25, 50]} totalRecords={data.length}
+        paginator rows={3} rowsPerPageOptions={[3, 5, 10]} totalRecords={data.length}
       >
         <Column field="id" header="ID" sortable></Column>
         <Column field="name" header="Name"></Column>
         <Column field="age" header="Age"></Column>
         <Column field="city" header="City"></Column>
       </DataTable>
+
+      <br />
+      <Password />
     </div>
   );
 }
---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.min.css";
+
+function login() {
+  return (
+      <div className="App">
+        <Password />
+      </div>
+  )
+}
+
+export default login
