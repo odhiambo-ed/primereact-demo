718c7b21c00541aa075d141d94bc011576150170
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-12-04 11:33:18 +0300

    add data.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
 
 function App() {
   return (
---
+++ src/data.js
@@ -0,0 +1,14 @@
+const data = [
+    { "id": 1, "name": "John Doe", "age": 30, "city": "New York" },
+    { "id": 2, "name": "Jane Doe", "age": 28, "city": "Los Angeles" },
+    { "id": 3, "name": "Bob Smith", "age": 35, "city": "Chicago" },
+    { "id": 4, "name": "Alice Johnson", "age": 25, "city": "San Francisco" },
+    { "id": 5, "name": "Charlie Brown", "age": 32, "city": "Seattle" },
+    { "id": 6, "name": "Eva Green", "age": 29, "city": "Miami" },
+    { "id": 7, "name": "David White", "age": 40, "city": "Boston" },
+    { "id": 8, "name": "Grace Miller", "age": 27, "city": "Austin" },
+    { "id": 9, "name": "Samuel Wilson", "age": 33, "city": "Denver" },
+    { "id": 10, "name": "Olivia Turner", "age": 31, "city": "Portland" }
+];
+
+export default data;
